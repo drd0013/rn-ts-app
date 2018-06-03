8a9a88984d578c2f59125008ba749a5748f60707
Rob Davis <rdavis@veeloinc.com>
2018-06-03 10:32:09 -0700

    render an image with a default dog, start of fetching

--- Dog.tsx
+++ Dog.tsx
@@ -1,23 +1,44 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { connect } from "react-redux";
+import { StyleSheet, Text, View, Image } from 'react-native';
+import { fetchDogUrl } from "./actions";
 
-export default class Dog extends React.Component {
+class Dog extends React.Component<any, any> {
+  constructor(props: any) {
+    super(props)
+
+  }
+
   componentDidMount() {
-    console.log('trigger action')
+    this.props.fetchDogUrl()
   }
 
   render() {
     return (
       <View style={styles.container}>
         <Text>Loading your new dog image!</Text>
+        <Image
+          style={styles.image}
+          source={{uri: this.props.dogUrl || 'https://images.dog.ceo/breeds/mountain-bernese/n02107683_5699.jpg'}}
+        />
       </View>
     );
   }
 }
 
+export default connect(
+    null,
+    {fetchDogUrl}
+)(Dog)
+
+
 //Connect state to props
 
 const styles = StyleSheet.create({
+  image: {
+    height: 400,
+    width: 300,
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
